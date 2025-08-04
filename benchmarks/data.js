7ffea072417bd3b2e9240c2f353c57cf3afb26c6
github-action-benchmark <github@users.noreply.github.com>
2025-08-04 15:31:58 +0000

    add Benchmark (cargo) benchmark result for 2b5f17e54d97e5d29ef77858e67d780a134a77b9

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1754318900713,
+  "lastUpdate": 1754321518209,
   "repoUrl": "https://github.com/shiyasmohd/ethrex",
   "entries": {
     "Benchmark": [
@@ -29,6 +29,36 @@ window.BENCHMARK_DATA = {
             "name": "Block import/Block import ERC20 transfers",
             "value": 165142559979,
             "range": "± 1335012816",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mrugiero@gmail.com",
+            "name": "Mario Rugiero",
+            "username": "Oppen"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2b5f17e54d97e5d29ef77858e67d780a134a77b9",
+          "message": "fix(l1): avoid broadcasting duplicate transactions (#3897)\n\n**Motivation**\n\nWire protocol correctness. Crucially, sending a transaction back to its\nsource is punishable by connection drop.\n\n**Description**\n\nAdd an early check for presence in the mempool for received\ntransactions, update the per-peer filters, and use them to avoid sending\nthem transactions they already know about in the `Transactions` relay\nand the `SendNewPooledTransactionHashes` paths.",
+          "timestamp": "2025-08-04T14:07:32Z",
+          "tree_id": "8d31b85b81f96d69ae44bddc24f467718c3d6c33",
+          "url": "https://github.com/shiyasmohd/ethrex/commit/2b5f17e54d97e5d29ef77858e67d780a134a77b9"
+        },
+        "date": 1754321516379,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "Block import/Block import ERC20 transfers",
+            "value": 163350198379,
+            "range": "± 276847874",
             "unit": "ns/iter"
           }
         ]
